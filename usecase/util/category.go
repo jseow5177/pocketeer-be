2a046583c99f2284d052da2fe9c4bd65fba7f104
jason.ngan <jason.ngan@shopee.com>
2023-05-20 10:31:14 +0800

    remove redundant funcs

--- usecase/util/category.go
+++ usecase/util/category.go
@@ -2,24 +2,6 @@ package util
 
 import "github.com/jseow5177/pockteer-be/entity"
 
-func GetCatIDs(categories []*entity.Category) []string {
-	ids := make([]string, len(categories))
-	for idx, cat := range categories {
-		ids[idx] = cat.GetCategoryID()
-	}
-	return ids
-}
-
-func GetIDToCategoryMap(categories []*entity.Category) map[string]*entity.Category {
-	idToCategoryMap := make(map[string]*entity.Category)
-
-	for _, cat := range categories {
-		idToCategoryMap[cat.GetCategoryID()] = cat
-	}
-
-	return idToCategoryMap
-}
-
 func GetCategoryIDToBudgetMap(
 	budgets []*entity.Budget,
 ) map[string]*entity.Budget {
