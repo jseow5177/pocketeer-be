ab2bd60ef586743807a26a8ec52fc22ea346d472
Jonathan Seow <jonathan.seowjn@shopee.com>
2023-08-29 21:58:06 +0800

    Use secret file

--- config/config.go
+++ config/config.go
@@ -1,31 +1,30 @@
 package config
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"net/url"
+	"os"
 	"time"
+
+	"github.com/rs/zerolog/log"
 )
 
 type Config struct {
-	Server              *Server      `json:"server"`
-	Mongo               *Mongo       `json:"mongo"`
-	Tokens              *Tokens      `json:"token"`
-	FinnHub             *FinnHub     `json:"finnhub"`
-	QuoteMemCache       *MemCache    `json:"quote_mem_cache"`
-	FeedbackGoogleSheet *GoogleSheet `json:"feedback_google_sheet"`
-	OTPMemCache         *MemCache    `json:"otp_mem_cache"`
-	Brevo               *Brevo       `json:"brevo"`
+	RateLimits          map[string]*RateLimit `json:"rate_limits"`
+	Mongo               *Mongo                `json:"mongo"`
+	Tokens              *Tokens               `json:"token"`
+	FinnHub             *FinnHub              `json:"finnhub"`
+	QuoteMemCache       *MemCache             `json:"quote_mem_cache"`
+	FeedbackGoogleSheet *GoogleSheet          `json:"feedback_google_sheet"`
+	OTPMemCache         *MemCache             `json:"otp_mem_cache"`
+	Brevo               *Brevo                `json:"brevo"`
 }
 
 type RateLimit struct {
 	RPS   float64 `json:"rps"`
 	Burst int64   `json:"burst"`
-}
-
-type Server struct {
-	LogLevel   string                `json:"log_level"`
-	Port       int                   `json:"port"`
-	RateLimits map[string]*RateLimit `json:"rate_limits"`
 }
 
 type MemCache struct {
@@ -89,11 +88,7 @@ type Brevo struct {
 
 func NewConfig() *Config {
 	return &Config{
-		Server: &Server{
-			LogLevel:   "DEBUG",
-			Port:       9090,
-			RateLimits: make(map[string]*RateLimit),
-		},
+		RateLimits: make(map[string]*RateLimit),
 		Mongo: &Mongo{
 			Username:       "pocketeer-test",
 			Password:       "twvD8CaUMd03WkdB",
@@ -126,7 +121,7 @@ func NewConfig() *Config {
 			PrivateKey:  "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCp6icfVAR2uJgV\nuqFoesHTY7M2nsbZb7glFdsPnC+7r3vJANxVs/kQUMGZyxJAu8YUKTEGFZUjsx8K\nM5gozspzs+vfjGi9cTsNkQLqoQcICZC10RVyl4vSDR4H/ihtIda2/8XI5wYYdvQM\nb84YoepaXLqBuwo2qUsecqEvolLD81S+8S873gK4vDHF1ceUMMZnt90U2JJ/DS+Y\nNrQTNu3o/POPZv3cwmVVu6ecgXHQco26jf1n6IaEcQhRiH7lBfCNkR6W7R5CUQBV\naDY+0zlC6iZyFJv5CTmULx/FCi9KrAmJE0IHb9dR8cxtUQjgWr3HOdxnyv+2EUf5\nRYyBx1jzAgMBAAECggEACPbO+JKkp6TGdUMC1/G9/wp//m4y/G6t7trv0yuAwyyX\nYbvXY/gaODeidxwlCuA9K3we13zVAOymwxGEwgZi3ObNl14fensaS/VuC3XSWqSy\niz0MHh8Lo9nIT1CjzloeK0pkI1y40BqadnuBioAkTn8c4dI90DNvQJx4j3xHVIoQ\nUelZCLz2qxzu3YQ6AGeE//Q30A2kTa3y1h5myFazDMdlmDYmBHUp17b/lUudPB9v\n4OXPu8j23AoyyT/4D5zJX05FvomaUpowboatc0MoDmgVwAu2Ug7MFAwOrey0tGvv\nmcZIvl6EQ6KpSFuoTwXqN2d7sjzq6H4Vi9TKpi6S/QKBgQDblQgMQLeqOcVumvTc\n9HcmF7RvbI6E7Yn4hMZTSnTSYcWM9ci/DfuiyuFLVYECacKWVgsrEjSSuUKwX01z\nbz8BXsB4bNVPii8qviWqEPubqk2+29bpQ9pZg8IBOe/jW9g0gP36ooxHt9Py2xG2\nVOSjvTdUkjeb+2kTcnWNt5qN7QKBgQDGGFmvNcqKZQE0Mj0r4dfOLvl8qvVJHIH0\ni1UQW1zTj5/FsuwoVABhHLo0ReBfbXMRGO/ppp26ogvj0LUw4dpCqId978vZz3RZ\n/7mb1+ifJXcc5+l8OttrfyVbHGskDUkJ9LrMXLAhAlYNxPhzs8K3ppgDir94gSn/\ncd7hEFmmXwKBgDSjypUdYqpVRSZZ0X+yv2mLXz8i+BuX0m6Ybe8Dt3PD6pb7SQ+8\nK2oAVvg3XEEW14YwxGaj66SM8xbTEf8tWR5b96om9RAnYV1Ozjqx7Y+IyTCLBT9Q\ne+TfuD+RAxgvKWqUzc4q75Q11oKuz9U1DsbOEpicoOYs5Ci8VMAPluaVAoGBAKdv\nuY5w5wtnKEdYF4BZ2jC6X8JSNhVf9TAf/PxgXOutQBy4iQflSJTM1U6NqYK/Xj0b\nWN2jKTqw8V/T7vKsU/F3xV5bK3Ck+vF/RwFE06iA4FccZqvMi94mkAqc0KqeWBgE\nNUe7KFweP2JQFLinPnRAacjEo+ZVNoxlUT/ms//9AoGBANbO4b59KoIS6txo/Tfd\n3+/90OsD8SlqI8XlFwjwV2ypOHP4vtHn24ZvuxWGxPj/jzsp8ZP9DQBCP6OiDaXG\nffQLJ6lCdkrKIuNXLrto4Sz5aNkgFnATKNH684ukuShgUz9s1wMkt599igzvA3hW\nCH8QzmeALOfcb2Ajl0N8LeRf",
 			Type:        "service_account",
 			SheetID:     "1xeY7_DRK0EYxEnO_3e9Wh7kNlgEZLIq0TL5EMth6juU",
-			WriteRange:  "Sheet1!A1:C2",
+			WriteRange:  "Sheet1!A1:D2",
 		},
 		OTPMemCache: &MemCache{
 			ExpiryTime:      "2m",
@@ -136,4 +131,27 @@ func NewConfig() *Config {
 			APIKey: "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",
 		},
 	}
+}
+
+func (c *Config) Subscribe(ctx context.Context, configFile string) error {
+	if configFile == "" {
+		log.Ctx(ctx).Warn().Msgf("empty config file")
+		return nil
+	}
+
+	f, err := os.Open(configFile)
+	if err != nil {
+		if os.IsNotExist(err) {
+			log.Ctx(ctx).Warn().Msgf("config file does not exist, file path: %v", configFile)
+			return nil
+		}
+		return err
+	}
+
+	p := json.NewDecoder(f)
+	if err := p.Decode(&c); err != nil {
+		return err
+	}
+
+	return nil
 }
